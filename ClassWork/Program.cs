5ed353313b307128bf6dfd259a87c2edc7bbce5c
Egor <dalinchikegor5@mail.ru>
2025-04-23 19:25:54 +0800

    Добавьте файлы проекта.

---
+++ ClassWork/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Code
+{
+    public static class Calculator
+    {
+        public static void Main()
+        {
+
+        }
+
+        public static double Sum(double digit1,  double digit2)
+        {
+            return digit1 + digit2;
+        }
+
+        public static double Minus(double digit1, double digit2)
+        {
+            return digit1 - digit2;
+        }
+    }
+}
---
+++ Tests/Test1.cs
@@ -0,0 +1,32 @@
+﻿using Code;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestSumm_PositiveNumber()
+        {
+            Assert.AreEqual(2, Calculator.Sum(1,1));
+        }
+
+        [TestMethod]
+        public void TestSumm_NegativeNumber()
+        {
+            Assert.AreEqual(-2, Calculator.Sum(-1, -1));
+        }
+
+        [TestMethod]
+        public void TestMinus_PositiveNumber()
+        {
+            Assert.AreEqual(1, Calculator.Minus(2, 1));
+        }
+
+        [TestMethod]
+        public void TestMinus_NegativeNumber()
+        {
+            Assert.AreEqual(-1, Calculator.Minus(-2, -1));
+        }
+    }
+}
